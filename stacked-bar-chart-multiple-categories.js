a13d67668faaa19420e9acc2a5c805dd002331aa
Nela Krawiecova <nela.krawiecova@gmail.com>
2020-09-17 12:43:54 +0200

    zeny-oprava

--- stacked-bar-chart-multiple-categories.js
+++ stacked-bar-chart-multiple-categories.js
@@ -29,7 +29,7 @@ Highcharts.chart('vis-stacked-bar-multiple-categories', {
       // title: {
       //     text: 'nepovolené jízdy za návěstidla'
       // },
-      max: 80,
+      min: 0,
       // showFirstLabel: false,
       labels: {
         formatter: function() {
